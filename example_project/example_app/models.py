3e04398f4a2f3f200e8cc618ffacf1e61b375c31
Mert Can Köseoğlu <mertckoseoglu@gmail.com>
2024-07-26 10:44:08 +0300

    first commit

--- example_project/example_app/models.py
+++ example_project/example_app/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length = 15)
     lastname = models.CharField(max_length = 15)
+    city = models.CharField(max_length = 15)

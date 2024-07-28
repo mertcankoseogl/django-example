07a40dc29e5f39c739326fa13f44e00f32f64751
Mert Can Köseoğlu <mertckoseoglu@gmail.com>
2024-07-28 23:37:45 +0300

    rearranged  variables and fields

--- db_exmpl/model_app/models.py
+++ db_exmpl/model_app/models.py
@@ -4,20 +4,20 @@ from django.db import models
 
 class USER(models.Model):
     Full_Name = models.CharField(max_length = 100, null=True, blank=True)
-    User_Name = models.CharField(max_length = 50, unique=True)
+    User_Name = models.CharField(max_length = 50, unique=True, null=True)
     Password = models.CharField(max_length = 128)
     Gender = models.CharField(max_length = 10, null=True, blank=True)
     Birthday = models.DateField(null=True, blank=True)
     Register_Date = models.DateField(auto_now_add = True)
     Biography = models.TextField(null=True, blank=True)
-    Email = models.EmailField(max_length = 200, unique=True)
+    Email = models.EmailField(max_length = 200, unique=True, null=True)
     User_Photo = models.ImageField(null=True, blank=True, upload_to = "images/")
 
 
 class CATEGORY(models.Model):
     Catg_Title = models.CharField(max_length = 50)
     Gender = models.CharField(max_length = 10)
-    Catg_Photo = models.ImageField(upload_to = "images/")
+    Catg_Photo = models.ImageField(null=True, blank=True, upload_to = "images/")
 
 
 class OUTFIT(models.Model):
@@ -26,7 +26,7 @@ class OUTFIT(models.Model):
     Title = models.CharField(max_length = 100, null=True, blank=True)
     Description = models.TextField(null=True, blank=True)
     Gender = models.CharField(max_length = 10)
-    Outfit_Photo = models.ImageField(upload_to = "images/")
+    Outfit_Photo = models.ImageField(null=True, blank=True, upload_to = "images/")
 
 
 class FAVORITE(models.Model):
@@ -38,7 +38,7 @@ class Part(models.Model):
     Outfit_Id = models.ForeignKey(OUTFIT, on_delete = models.CASCADE)
     Part_Name = models.CharField(max_length = 50)
     Link = models.URLField()
-    Subtitle = models.TextField(max_length = 250)
+    Subtitle = models.TextField()
 
 
 class FOLLOWER(models.Model):

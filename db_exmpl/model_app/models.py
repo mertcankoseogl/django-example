bc279c4df668f7b84236804adf9fdd18f60b8253
Mert Can Köseoğlu <mertckoseoglu@gmail.com>
2024-07-28 17:42:49 +0300

    Add user_photo and outfit_photo to models

--- db_exmpl/model_app/models.py
+++ db_exmpl/model_app/models.py
@@ -5,23 +5,25 @@ from django.db import models
 class USER(models.Model):
     Full_Name = models.CharField(max_length = 100)
     User_Name = models.CharField(max_length = 50)
-    Password = models.CharField(max_length = 25)
+    Password = models.CharField(max_length = 128)
     Gender = models.CharField(max_length = 10)
     Birthday = models.DateField()
-    Register_Date = models.DateField()
-    Biography = models.TextField()
-    Email = models.EmailField()
+    Register_Date = models.DateField(auto_now_add = True)
+    Biography = models.TextField(max_length = 250)
+    Email = models.EmailField(max_length = 200)
+    User_Photo = models.ImageField(null=True, blank=True, upload_to = "images/")
 
 class CATEGORY(models.Model):
-    Catg_Title = models.TextField()
+    Catg_Title = models.CharField(max_length = 50)
     Gender = models.CharField(max_length = 10)
 
 class OUTFIT(models.Model):
-    CategoryId = models.ForeignKey(CATEGORY, on_delete = models.CASCADE)
+    Category_Id = models.ForeignKey(CATEGORY, on_delete = models.CASCADE)
     User_Id = models.ForeignKey(USER, on_delete = models.CASCADE)
     Title = models.TextField()
     Description = models.TextField()
     Gender = models.CharField(max_length = 10)
+    Outfit_Photo = models.ImageField(null=True, blank=True, upload_to = "images/")
 
 
 class FAVORITE(models.Model):
@@ -30,9 +32,9 @@ class FAVORITE(models.Model):
 
 class Part(models.Model):
     Outfit_Id = models.ForeignKey(OUTFIT, on_delete = models.CASCADE)
-    Part_Name = models.CharField(50)
-    Link = models.CharField(250)
-    Subtitle = models.TextField()
+    Part_Name = models.CharField(max_length = 50)
+    Link = models.CharField(max_length = 250)
+    Subtitle = models.TextField(max_length = 250)
 
 class FOLLOWER(models.Model):
     Follower_Id = models.ForeignKey(USER, on_delete = models.CASCADE)
